aec7ef90fb7ff196760b3a538369c16595aca502
Maryna Yudziankova <Maryna_Yudziankova@epam.com>
2019-01-17 11:27:57 +0300

    remote

--- MyShows/MyShows/Lib/Driver/Browser.cs
+++ MyShows/MyShows/Lib/Driver/Browser.cs
@@ -1,6 +1,8 @@
 ﻿using MyShows.Lib.Driver;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Remote;
 using System;
 using System.Drawing;
 
@@ -27,7 +29,11 @@ namespace Framework.Lib.Driver
 
         private Browser()
         {
-            WebDriver = new ChromeDriver("D:\\study\\MyShows\\resources");
+            FirefoxOptions options = new FirefoxOptions();
+            options.PlatformName = "linux";
+
+            //WebDriver = new ChromeDriver("D:\\study\\MyShows\\resources");
+            WebDriver = new RemoteWebDriver(new Uri("http://l192.168.0.104:4444/wd/hub"), options.ToCapabilities(), TimeSpan.FromSeconds(120));
             WebDriver.Manage().Window.Size = new Size(1920, 1080);
         }
 
